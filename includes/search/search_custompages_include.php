d1b868a5d0bd76ed062631b262c7ed35c6c44229
Patrick Conrad <conrad@iza.org>
2016-07-12 16:21:36 +0200

    Rewrite Search for Bootstrap

--- includes/search/search_custompages_include.php
+++ includes/search/search_custompages_include.php
@@ -23,6 +23,7 @@ if ($_GET['stype'] == "custompages" || $_GET['stype'] == "all") {
 	$sortby = "page_title";
 	$ssubject = search_querylike("page_title");
 	$smessage = search_querylike("page_content");
+
 	if ($_GET['fields'] == 0) {
 		$fieldsvar = search_fieldsvar($ssubject);
 	} else if ($_GET['fields'] == 1) {
@@ -32,18 +33,23 @@ if ($_GET['stype'] == "custompages" || $_GET['stype'] == "all") {
 	} else {
 		$fieldsvar = "";
 	}
+
+	$rows = 0;
 	if ($fieldsvar) {
 		$result = dbquery("SELECT * FROM ".DB_CUSTOM_PAGES." WHERE ".groupaccess('page_access')." AND ".$fieldsvar);	 
 		$rows = dbrows($result);
-	} else {
-		$rows = 0;
 	}
+
 	if ($rows != 0) {
-		$items_count .= THEME_BULLET."&nbsp;<a href='".FUSION_SELF."?stype=custompages&amp;stext=".$_GET['stext']."&amp;".$composevars."'>".$rows." ".($rows == 1 ? $locale['c401'] : $locale['c402'])." ".$locale['522']."</a><br />\n";
+		$items_count .= THEME_BULLET."&nbsp;"
+            ."<a href='".FUSION_SELF."?stype=custompages&amp;stext=".$_GET['stext']."&amp;".$composevars."'>"
+            .$rows." ".($rows == 1 ? $locale['c401'] : $locale['c402'])." ".$locale['522']
+            ."</a><br />\n";
 		$result = dbquery(
 			"SELECT * FROM ".DB_CUSTOM_PAGES."
 			WHERE ".groupaccess('page_access')." AND ".$fieldsvar."
-			ORDER BY ".$sortby." ".($_GET['order'] == 1 ? "ASC" : "DESC").($_GET['stype'] != "all" ? " LIMIT ".$_GET['rowstart'].",10" : "")
+			ORDER BY ".$sortby." ".($_GET['order'] == 1 ? "ASC" : "DESC")
+            .($_GET['stype'] != "all" ? " LIMIT ".$_GET['rowstart'].",10" : "")
 		);	 
 		while ($data = dbarray($result)) {
 			$search_result = "";
@@ -56,12 +62,19 @@ if ($_GET['stype'] == "custompages" || $_GET['stype'] == "all") {
 			$text_frag = search_textfrag($text_all);
 			$subj_c = search_stringscount($data['page_title']);
 			$text_c = search_stringscount($text_all);
-			// $text_frag = highlight_words($swords, $text_frag);
-			$search_result .= "<a href='viewpage.php?page_id=".$data['page_id']."'>".$data['page_title']."</a>"."<br /><br />\n";
-			// $search_result .= "<a href='viewpage.php?page_id=".$data['page_id']."'>".highlight_words($swords, $data['page_title'])."</a>"."<br /><br />\n";
-			$search_result .= "<div class='quote' style='width:auto;height:auto;overflow:auto'>".$text_frag."</div><br />\n";
-			$search_result .= "<span class='small'>".$subj_c." ".($subj_c == 1 ? $locale['520'] : $locale['521'])." ".$locale['c403']." ".$locale['c404'].", ";
-			$search_result .= $text_c." ".($text_c == 1 ? $locale['520'] : $locale['521'])." ".$locale['c403']." ".$locale['c405']."</span><br /><br />\n";
+
+            $search_result .= "<div class='media'>\n<div class='media-body'>\n";
+            $search_result .= "<h4 class='media-heading'>"
+                ."<a href='".BASEDIR."viewpage.php?page_id=".$data['page_id']."'>".$data['page_title']."</a>"
+                ."</h4>\n";
+            $search_result .= "<div class='media-text' style='width:auto; height:auto; overflow:auto'>\n<p>\n"
+                .$text_frag
+                ."</p>\n<span class='small text-muted'>"
+                .$subj_c." ".($subj_c == 1 ? $locale['520'] : $locale['521'])." ".$locale['c403']." ".$locale['c404'].", "
+                .$text_c." ".($text_c == 1 ? $locale['520'] : $locale['521'])." ".$locale['c403']." ".$locale['c405']
+                ."</span>\n</div>\n";
+            $search_result .= "</div>\n</div>\n<hr />\n";
+
 			search_globalarray($search_result);
 		}
 	} else {
